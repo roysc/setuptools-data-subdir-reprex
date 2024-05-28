ae19eabde84447cb01bbc418134081dad60bed56
Roy Crihfield <roy@manteia.ltd>
2024-05-28 18:14:35 +0800

    try find_namespace_packages

--- setup.py
+++ setup.py
@@ -1,8 +1,9 @@
 from setuptools import setup, find_packages
+from setuptools import find_namespace_packages
 
 setup(
     name='debug-datadirs',
     version='0.1.0',
-    packages=find_packages(),
+    packages=find_namespace_packages(),
     package_data={'': ['data/**']},
 )
